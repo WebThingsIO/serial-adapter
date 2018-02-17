b573b25ff802b66d8629af331e6d087d74413694
Dave Hylands <dhylands@gmail.com>
2018-02-16 16:41:56 -0800

    UPdated README.md and added list-ports.js

--- README.md
+++ README.md
@@ -1,8 +1,41 @@
 Adapter for talking to serial-mcu based software through a serial port.
 
-This respository is a place-holder for the adapter code which will talk to simple mcu devices (like an Arduino) through a serial interface.
+# Configuring the serial port.
 
-These are the set of message currently needing to be transferred:
+The configuration for this adapter is current defaulted to:
+```
+    "config": {
+      "ports": {
+        "arduino-usb": {
+          "manufacturer": "Arduino"
+        }
+      }
+    }
+```
+which will scan the available serial ports and look for a USB port with a
+manufacterer of 'Arduino', which is what the MKR1000 advertises itself with.
+
+Each entry under `ports` is treated like a filter. The name (i.e. `arduino-usb`) is arbitrary. Each filter can have one of more of the following fields:
+
+| Name | Description |
+| ---- | ----------- |
+| manufacturer | Matches the manufacturer name for USB serial ports. |
+| vendorId | Matches the 4 character hex vendor ID associated with a USB serial port |
+| productId | Matches the 4 character hex product ID associated with a USB serial port |
+| serialNumber | Matches the serial number associated with a USB serial port |
+| comName | Matches the name of the serial port (this works for both USB and non-USB serial ports) |
+
+Each of the filters checks the actual string starts with the string provided in the filter. For example, the MKR1000 advertises a manufacturer of `Arduino_LLC` and since it starts with `Arduino` the `arduino-usb`
+filter will match and that serial port will be opened.
+
+There is a Node.js program called list-ports.js which will print all of
+ports which were detected as well as which ports match each of the filters
+from the config found in the package.json file.
+
+# Some random notes.
+
+These are the set of messages currently needing to be transferred (each
+message is formatted using JSON):
 
 ```
 Sent '{"messageType":"getAdapter","data":{}}'
---
+++ list-ports.js
@@ -0,0 +1,99 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const SerialPort = require('serialport');
+const utils = require('./utils');
+
+function serialPortMatches(port, portsConfig) {
+  // We only filter using keys from the following:
+  const compareKeys = ["manufacturer",
+                       "vendorId",
+                       "productId",
+                       "serialNumber",
+                       "comName"];
+
+  // Under OSX, SerialPort.list returns the /dev/tty.usbXXX instead
+  // /dev/cu.usbXXX. tty.usbXXX requires DCD to be asserted which
+  // isn't necessarily the case for usb-to-serial dongles.
+  // The cu.usbXXX doesn't care about DCD.
+  if (port.comName.startsWith('/dev/tty.usb')) {
+    port.comName = port.comName.replace('/dev/tty', '/dev/cu');
+  }
+  for (const name in portsConfig) {
+    const portConfig = portsConfig[name];
+
+    const configKeys = Object.keys(portConfig)
+                             .filter(ck => compareKeys.indexOf(ck) >= 0);
+    if (configKeys.length == 0) {
+      // No keys - it doesn't match
+      continue;
+    }
+    let match = true;
+    for (const configKey of configKeys) {
+      let configVal = portConfig[configKey];
+      let portVal = port[configKey];
+      if (typeof(portVal) != 'string' || !portVal.startsWith(configVal)) {
+        match = false;
+      }
+    }
+    if (match) {
+      // All of the fields from the config match the values from the port,
+      // so we have a match;
+
+      // If there is a baudRate, copy it over
+      if (portConfig.hasOwnProperty('baudRate')) {
+        port.baudRate = portConfig.baudRate;
+      } else {
+        port.baudRate = 115200;
+      }
+      return true;
+    }
+  }
+  return false;
+}
+
+function printPorts(ports) {
+  let lines = [
+    ['comPort', 'vendorId', 'productId', 'serialNumber', 'manufacturer'],
+    '-'
+  ];
+
+  for (let port of ports) {
+    if (port.comName.startsWith('/dev/tty.usb')) {
+      port.comName = port.comName.replace('/dev/tty', '/dev/cu');
+    }
+    lines.push([port.comName,
+                port.vendorId || '',
+                port.productId || '',
+                port.serialNumber || '',
+                port.manufacturer || '']);
+  }
+
+  utils.printTable('<<<<<', lines);
+}
+
+SerialPort.list().then(ports => {
+  // First print a list of all of the ports
+  console.log('===== All ports found =====');
+  printPorts(ports);
+
+  const manifestData = fs.readFileSync('package.json');
+  const manifest = JSON.parse(manifestData);
+  let portsConfig = manifest.moziot &&
+                    manifest.moziot.config &&
+                    manifest.moziot.config.ports;
+  if (!portsConfig) {
+    console.log('No moziot.config.ports found in package.json');
+    return;
+  }
+
+  for (let name in portsConfig) {
+    console.log('');
+    console.log('===== Serial ports which match filter', name, '=====');
+    let portConfig = {};
+    portConfig[name] = portsConfig[name];
+    let matchingPorts =
+      ports.filter(port => serialPortMatches(port, portConfig));
+    printPorts(matchingPorts);
+  }
+});
